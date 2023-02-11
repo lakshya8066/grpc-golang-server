ae357980315a27fa8a371a9dfb1758b38b16f0e4
Lakshya Gupta <lakshya.gupta.1999@gmail.com>
2023-02-11 15:42:52 +0530

    Add client
    Forgot to add client files in previous commit

--- client/main.go
+++ client/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"log"
+
+	pb "github.com/lakshya8066/grpc-golang-server/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const port = ":8080"
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Could not connect %v", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	// names := &pb.NamesList{
+	// 	Names: []string{"Raman", "Aman", "Naman"},
+	// }
+
+	callSayHello(client)
+}

a9593512e74ec4f2ab4d78c0fad2fd0d24c166dc
Lakshya Gupta <lakshya.gupta.1999@gmail.com>
2023-02-11 15:10:21 +0530

    Server files setup

--- server/main.go
+++ server/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/lakshya8066/grpc-golang-server/proto"
+	"google.golang.org/grpc"
+)
+
+type helloServer struct {
+	pb.GreetServiceServer
+}
+
+const port = ":8080"
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatalf("Failed to start the server %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	log.Printf("Greet Server started at %v", lis.Addr())
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to start: %v", err)
+	}
+}
